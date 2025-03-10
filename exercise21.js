e36ef8fbcc00a94dc1a6d2ff6774eb702f09c866
Mike FM <167895558+MikeeFdez@users.noreply.github.com>
2025-03-10 12:44:16 +0100

    Create exercise21.js

---
+++ exercise21.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Object | null} tree - El árbol binario
+ * @returns {number} - La altura del árbol
+ */
+function treeHeight(tree) {
+  if (!tree) return 0; // Un árbol vacío tiene altura 0
+  
+  // Recursión: tomamos el máximo entre la altura del lado izquierdo y derecho
+  return 1 + Math.max(treeHeight(tree.left), treeHeight(tree.right));
+}
